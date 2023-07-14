7a11447ee405cb24f475d3e09eaf1f3de8d487b1
matheus_mai <matheus.pereira@neoprospecta.com>
2023-07-14 15:18:43 -0300

    Adição dos bash

---
+++ grep_genes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Pasta com os arquivos gerados
+
+cd ~/Trabalho_TCC/dbNSFP4.2a/
+mkdir Genes/
+pasta="/home/matheus_mai/Trabalho_TCC/dbNSFP4.2a"
+
+# Arquivo contendo os nomes a serem buscados
+arquivo_nomes="/home/matheus_mai/Trabalho_TCC/genes_utilizados.csv"
+
+while IFS=$',' read -r chr nome; do
+    echo "Procurando por: $nome no arquivo relacionado ao chr $chr"
+
+    # Nome do arquivo relacionado ao valor de "chr"
+    arquivo="${pasta}/dbNSFP4.2a_variant.chr${chr}_cortado.csv"
+
+    # Verificar se o arquivo existe
+    if [ -f "$arquivo" ]; then
+        # Nome da pasta para salvar o arquivo de ocorrências
+        pasta_destino="${pasta}/Genes/chr_${chr}"
+
+        # Criar a pasta de destino se ainda não existir
+        mkdir -p "$pasta_destino"
+
+        # Remover tudo após o primeiro ' ; ' na coluna 11
+        awk -F'\t' '{gsub(/;.*$/, "", $11); if ($11 == "'"$nome"'") print}' "$arquivo" > "${pasta_destino}/${nome}_ocorrencia.csv"
+
+        echo "Busca concluída para o nome: $nome"
+    else
+        echo "Arquivo não encontrado para o chr $chr"
+    fi
+done < "$arquivo_nomes"
+
+echo "Processo concluído."
