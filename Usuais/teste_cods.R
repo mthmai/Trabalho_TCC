2e33c890b390a7931ea7300337122eab93361560
matheus_mai <matheus.pereira@neoprospecta.com>
2024-01-14 15:29:17 -0300

    Adição de dados para o R

--- Usuais/teste_cods.R
+++ Usuais/teste_cods.R
@@ -2,16 +2,16 @@ library(car)
 library(emmeans)
 library(multcomp)
 
-dados <- read.table("/home/matheus_mai/Desktop/complet_table_TCC_test.csv", header = TRUE, sep = ";")
+dados <- read.table("/home/matheus_mai/Trabalho_TCC/Usuais/testando_1_2_3.csv", header = TRUE, sep = ";")
 print(dados)
 
 # Verificar se a coluna "coluna_nome" contém valores inteiros
 # Verificar se a coluna "coluna_nome" contém valores strings
 # Substituir vírgula por ponto na coluna "coluna_nome"
-dados$Youden <- as.numeric(gsub(",", ".", dados$Youden))
+#dados$Youden <- as.numeric(gsub(",", ".", dados$Youden))
 
-is_int <- sapply(dados$Youden, is.numeric)
-print(is_int)
+#is_int <- sapply(dados$Youden, is.numeric)
+#print(is_int)
 
 
 
@@ -60,7 +60,7 @@ print(pairs_bh[1:200, ])
 
 #print a comparação entre grupo ordenados 
 print(cld_result[1:200, ])
-print(cld_result[201:400, ])
+print(cld_result[201:304, ])
 print(cld_result[401:800, ])
 print(cld_result[801:1300, ])
 print(cld_result[1301:1500, ])
@@ -68,4 +68,25 @@ print(cld_result[1501:1800, ])
 print(cld_result[1801:2015, ])
 
 
+#Gráfico de barras
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(cld_result, aes(x = reorder(programs, emmean), y = emmean, fill = PANTHER_CLASS)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Comparação entre Grupos Ordenados",
+       x = "Programas",
+       y = "Média dos Valores",
+       fill = "PANTHER_CLASS") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+#Gráfico de pontos
+install.packages("ggplot2")
+library(ggplot2)
+
+ggplot(cld_result, aes(x = reorder(programs, emmean), y = emmean, color = PANTHER_CLASS)) +
+  geom_point() +
+  labs(title = "Comparação entre Grupos Ordenados",
+       x = "Programas",
+       y = "Média dos Valores",
+       color = "PANTHER_CLASS") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
